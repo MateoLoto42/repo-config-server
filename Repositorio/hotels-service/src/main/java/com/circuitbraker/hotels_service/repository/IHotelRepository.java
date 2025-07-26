3bfc45d197b6d717c51530d2e0ba7399d8c128e0
MateoLoto42 <matuteloto42@gmail.com>
2025-07-26 12:29:54 -0300

    Delete Repositorio directory

--- Repositorio/hotels-service/src/main/java/com/circuitbraker/hotels_service/repository/IHotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.circuitbraker.hotels_service.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IHotelRepository {
-}
--- Repositorio/hotels-service/src/main/java/com/circuitbraker/hotels_service/service/IHotelService.java
+++
@@ -1,9 +0,0 @@
-package com.circuitbraker.hotels_service.service;
-
-import com.circuitbraker.hotels_service.model.Hotel;
-
-import java.util.List;
-
-public interface IHotelService {
-    public List<Hotel> getHotelsByCityId(Long city_id);
-}
